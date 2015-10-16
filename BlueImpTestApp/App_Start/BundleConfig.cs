07e3d7631705514c4aa2342cd416646558d9d113
Toy, Patrick <patrick.toy@intelsat.com>
2015-10-16 11:31:45 -0400

    Added BlueImp file uploader

--- BlueImpTestApp/App_Start/BundleConfig.cs
+++ BlueImpTestApp/App_Start/BundleConfig.cs
@@ -23,21 +23,26 @@ namespace BlueImpTestApp
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryfileupload").Include(
+                        "~/Scripts/jquery.ui.widget.js",
+                        "~/Scripts/jquery.iframe-transport.js",
+                        "~/Scripts/jquery.fileupload.js"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Styles/css").Include("~/Styles/site.css"));
+
+            bundles.Add(new StyleBundle("~/Styles/themes/base/css").Include(
+                        "~/Styles/themes/base/jquery.ui.core.css",
+                        "~/Styles/themes/base/jquery.ui.resizable.css",
+                        "~/Styles/themes/base/jquery.ui.selectable.css",
+                        "~/Styles/themes/base/jquery.ui.accordion.css",
+                        "~/Styles/themes/base/jquery.ui.autocomplete.css",
+                        "~/Styles/themes/base/jquery.ui.button.css",
+                        "~/Styles/themes/base/jquery.ui.dialog.css",
+                        "~/Styles/themes/base/jquery.ui.slider.css",
+                        "~/Styles/themes/base/jquery.ui.tabs.css",
+                        "~/Styles/themes/base/jquery.ui.datepicker.css",
+                        "~/Styles/themes/base/jquery.ui.progressbar.css",
+                        "~/Styles/themes/base/jquery.ui.theme.css"));
         }
     }
 }
---
+++ BlueImpTestApp/build-everything-prod.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /verbosity:n /t:Build /p:BuildingFor=Production BlueImpTestApp.csproj /p:nowarn="1572,1573,1574,1584,1591,1658"
