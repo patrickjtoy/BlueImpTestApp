725e604971b54f4c56e2b35dfbddcf0bb745e70c
Alexander Ziskind <alexziskind1@yahoo.com>
2015-10-16 14:38:31 -0400

    implementedf server side code

---
+++ BlueImpTestApp/Models/DbDocument.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+namespace BlueImpTestApp.Models
+{
+    public class DbDocument
+    {
+        public int Id { get; set; }
+
+        public string Location { get; set; }
+
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public int FileSize { get; set; }
+
+        public byte[] FileData { get; set; }
+
+        public int CreatedByUserId { get; set; }
+        public int DeletedByUserId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
+        public int DocumentTypeId { get; set; }
+
+    }
+
+}
---
+++ BlueImpTestApp/Models/DocumentTypeModel.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace BlueImpTestApp.Models
+{
+    public class DocumentTypeModel
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    }
+}
